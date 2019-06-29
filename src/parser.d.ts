6e4c2b45907e7aa50ab7965674f370b78a4424fc
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-29 10:01:43 +0900

    add type declaration file of parser.

---
+++ src/parser.d.ts
@@ -0,0 +1,44 @@
+export type PegjsTable = {
+    defs: Map<string, Location>;
+    refs: Map<string, Location[]>;
+}
+
+export type Location = {
+    start: {
+        offset: number;
+        line: number;
+        column: number;
+    };
+    end: {
+        offset: number;
+        line: number;
+        column: number;
+    };
+}
+
+export declare class SyntaxError extends Error {
+    message: string;
+    expected: string | null;
+    found: string | null;
+    location: Location;
+    name : 'SyntaxError';
+}
+
+export type ParserOptions = {
+    startRule?: 'Root' | 'Preamble';
+    tracer?: Tracer;
+    enableComment?: boolean;
+}
+
+export type TraceArg = {
+    type: 'rule.enter' | 'rule.match' | 'rule.fail';
+    rule: string;
+    result: string | Node;
+    location: Location;
+}
+
+export type Tracer = {
+    trace: (e: TraceArg) => any;
+}
+
+export declare function parse(input: string, options?: ParserOptions): PegjsTable;
